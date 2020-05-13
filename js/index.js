aa30a77b17f470749a918e6928af59414f7f3e2d
Dinislam Sadigov <51309582+azerzet@users.noreply.github.com>
2020-05-14 01:01:24 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -65,3 +65,23 @@ var todolist = new Vue({
         }
     }
 });
+
+// Дальше Код к Индексу
+
+const switchTumblerHandler = () => {
+  const wrapper = document.querySelector('.tumbler__wrapper')
+  
+  wrapper.addEventListener('click', () => {
+    toggleNightMode()
+  })
+}                          
+
+const toggleNightMode = () => {
+  document.body.classList.toggle('body--night-mode')
+  document.querySelector('.tumbler').classList.toggle('tumbler--night-mode')
+  document.querySelectorAll('.post').forEach(post => {
+    post.classList.toggle('post--night-mode')
+  })
+}
+
+switchTumblerHandler()
