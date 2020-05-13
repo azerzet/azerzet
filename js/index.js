311e5ab82fa0d159648c8c7a3559db06d77550f6
Dinislam Sadigov <51309582+azerzet@users.noreply.github.com>
2020-05-14 01:24:51 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-const switchTumblerHandler = () => {
-  const wrapper = document.querySelector(".tumbler__wrapper");
-
-  wrapper.addEventListener("click", () => {
-    toggleNightMode();
-  });
-};
-
-const toggleNightMode = () => {
-  document.body.classList.toggle("body--night-mode");
-  document.querySelector(".tumbler").classList.toggle("tumbler--night-mode");
-  document.querySelectorAll(".post").forEach(post => {
-    post.classList.toggle("post--night-mode");
-  });
-};
-
-switchTumblerHandler();
-
-// Дальше Код к Notes
-
 Vue.component('togglebutton', {
     props: ['label', 'name'],
     template: `<div class="togglebutton-wrapper" v-bind:class="isactive ? 'togglebutton-checked' : ''">
